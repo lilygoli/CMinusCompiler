caf0a91c7b11e1c4b7d2612c659bcb8b41db1834
l <leilygoli100@gmail.com>
2021-04-16 23:25:25 +0430

    Start of Phase 2: Parser class added. (ps: Sasa copyright reserved :))) )

--- parser.py
+++ parser.py
@@ -1,4 +1,6 @@
-
+from File_Reader import FileReader
+from Scanner import Scanner
+from anytree import Node, RenderTree
 
 
 def get_dictionary(file_name):
@@ -25,14 +27,63 @@ def get_dictionary(file_name):
             info[rule_id] = l
     return info
 
+
 first = get_dictionary("first_sets")
 follow = get_dictionary("follow_sets")
 
-lili_command = ["B", "H", "Statement", "Statementlist"]
+# lili_command = ["B", "H", "Statement", "Statementlist"]
+#
+# for i in lili_command:
+#     print(i, "__________")
+#     print("first set is")
+#     print(first[i])
+#     print("follow set is")
+#     print(follow[i])
 
-for i in lili_command:
-    print(i, "__________")
-    print("first set is")
-    print(first[i])
-    print("follow set is")
-    print(follow[i])
+
+class Parser():
+    def __init__(self, scanner):
+        self.scanner = scanner
+        self.line_number = 0
+        self.cur_token = ''
+        self.get_next_token()
+
+    def get_next_token(self):
+        self.cur_token = self.scanner.get_next_token()
+        self.line_number = self.scanner.f.lineno
+
+    def add_edge(self, a, b):
+        Node(b, parent=a)
+
+    def print_error_follow(self, name):
+        print("missing" + name + " in line " + self.line_number)
+
+    def print_error_illegal(self, LA):
+        print("illegal" + LA + " in line " + self.line_number)
+
+    def match(self, expected):
+        if self.cur_token == expected:
+            self.get_next_token()
+        else:
+            print("missing "+ expected + " in line "+ self.line_number)
+
+    def Program(self):
+        LA = self.cur_token
+        if LA in first["Program"]:
+            self.add_edge("Declaration_list", "Program")
+            self.Declaration_list()
+        elif LA in follow["Program"]:
+            self.print_error_follow("Program")
+        else:
+            self.print_error_illegal(LA)
+            self.get_next_token()
+
+    def Declaration_list(self):
+        pass
+
+
+f = FileReader("test.txt")
+s = Scanner(f)
+p = Parser(s)
+Node("Program")
+p.Program()
